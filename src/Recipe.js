5e3feb905708f34e921ec631c16989147c4c87d0
era-muriithi <eramuriithi@gmail.com>
2020-05-03 17:36:09 +0300

    change spacing

--- src/Recipe.js
+++ src/Recipe.js
@@ -5,15 +5,14 @@ import style from "./recipe.module.css"
 const Recipe = ({title, calories, image, ingredients}) => {
     return(
         <div className={style.recipe}>
-        <h1>{title}</h1>
-        <ul className={style.ingredients}>
-        {ingredients.map(ingredient =>(
-            <li>{ingredient.text}</li>
-         ))}
-        </ul>
+            <h1>{title}</h1>
+            <ul className={style.ingredients}>
+                {ingredients.map(ingredient =>(
+                <li>{ingredient.text}</li>
+                ))}
+            </ul>
         <p>{calories}</p>
         <img className={style.image} src={image} alt=""/>
-
         </div>
 
     );
